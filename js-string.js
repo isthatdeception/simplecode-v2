e4b38e3761978665bc0e69c169e8b83cf7d8997b
Aditya Mehra <aditya97mehra@gmail.com>
2025-05-09 16:51:00 +0530

    completed multiple excercises

---
+++ js-string.js
@@ -0,0 +1,8 @@
+console.log("My name is : " + "Aditya Mehra");
+
+console.log("Total cost : " + "$" + (5 + 3));
+
+console.log(`Total cost : $${5 + 3}`);
+
+alert(`Total cost : $${5 + 3}`);
+hddssssssssssssssss
