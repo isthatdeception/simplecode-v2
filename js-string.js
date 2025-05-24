1017cde7577f7ce982f7fb07fdb93983c586428c
Aditya Mehra <aditya97mehra@gmail.com>
2025-05-24 15:12:46 +0530

    bug fixes

--- js-string.js
+++ js-string.js
@@ -4,5 +4,4 @@ console.log("Total cost : " + "$" + (5 + 3));
 
 console.log(`Total cost : $${5 + 3}`);
 
-alert(`Total cost : $${5 + 3}`);
-hddssssssssssssssss
+alert(`Total cost of the calculation is: $${5 + 3}`);
