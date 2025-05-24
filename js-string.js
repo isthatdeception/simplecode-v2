14a17d2f5876a63fee1eac5350a89e60a063ccdd
Aditya Mehra <aditya97mehra@gmail.com>
2025-05-24 21:57:50 +0530

    fixes

--- js-string.js
+++
@@ -1,7 +0,0 @@
-console.log("My name is : " + "Aditya Mehra");
-
-console.log("Total cost : " + "$" + (5 + 3));
-
-console.log(`Total cost : $${5 + 3}`);
-
-alert(`Total cost of the calculation is: $${5 + 3}`);
